8360aa4380d7b259b7f0339881d0161462f8c1ba
SmellyCat <smellycat2014@foxmail.com>
2017-11-24 12:09:23 +0800

    更新 默认关闭subscribe

--- helper_kafka_consumer.py
+++ helper_kafka_consumer.py
@@ -17,7 +17,14 @@ class KafkaConsumer(object):
             'default.topic.config': {'auto.offset.reset': 'smallest'},
             'enable.auto.commit': "false"
         })
-        self.c.subscribe([kwargs['topic']])
+        #self.c.subscribe([kwargs['topic']])
+        self.topic = kwargs['topic']
+
+    def assign(self, part_list):
+        partitions = []
+        for i in part_list:
+            partitions.append(TopicPartition(self.topic, i))
+        self.c.assign(partitions)
 
     def __del__(self):
         self.c.close()
